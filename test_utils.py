a1eb84eaab1a5a977b1ccd63a69730005ac0bb49
Regina <keriregina@gmail.com>
2024-04-05 11:11:56 +0200

    Update style

--- test_utils.py
+++ test_utils.py
@@ -1,5 +1,6 @@
 from utils import add
 
+
 def test_add():
     assert add(1, 2) == 3
     assert add(5, 5) == 10
--- utils.py
+++ utils.py
@@ -1,6 +1,3 @@
-# hier werde ih igrendwas programmieren
-
 def add(a, b):
     """Add two numbers together"""
     return a + b
-
