150bd055f46e2be7df931ebcb30def99514ea94f
Regina <keriregina@gmail.com>
2024-04-07 22:16:07 +0200

    Update test_utils

--- test_utils.py
+++ test_utils.py
@@ -4,11 +4,12 @@ In this module the functions of utils.py are tested
 
 
 import utils
+import math
 
 
 def test_calc_crf():
     '''Test the function calc_crf'''
-    assert round(utils.calc_crf(0.02, 25), 3) == 0.071
+    assert math.isclose(utils.calc_crf(10, 10), 10, abs_tol = 0.001)
 
 def test_calc_self_consumption():
     '''Test the function calc_self_consumption'''
@@ -21,8 +22,3 @@ def test_calc_annual_costs():
 def test_calc_annual_revenues():
     '''Test the function calc_annual_revenues'''
     assert utils.calc_annual_revenues(800, 0.1) == 80
-
-# def test_calc_npv():
-#     '''Test the function calc_npv'''
-#     assert utils.calc_npv(1000, 0.02, 2, 25, 800, 2000, 0.1, 0,1)
-#     utils.calc_npv(investment_costs, int_rate, installed_capacity, lifetime, full_load_hours, annual_load, feedin_tarif, supply_tarif)
