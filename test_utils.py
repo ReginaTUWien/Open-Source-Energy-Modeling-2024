69649efd6749511b2f72bbe9c9cd00c17130fbe1
Regina <keriregina@gmail.com>
2024-04-07 22:56:43 +0200

    Update all files

--- test_utils.py
+++ test_utils.py
@@ -8,20 +8,20 @@ import math
 
 
 def test_calc_crf():
-    '''Test the function calc_crf'''
+    """Test the function calc_crf"""
     assert math.isclose(utils.calc_crf(10, 10), 10, abs_tol=0.001)
 
 
 def test_calc_self_consumption():
-    '''Test the function calc_self_consumption'''
+    """Test the function calc_self_consumption"""
     assert utils.calc_self_consumption(2, 800) == 800
 
 
 def test_calc_annual_costs():
-    '''Test the function calc_annual_costs'''
+    """Test the function calc_annual_costs"""
     assert utils.calc_annual_costs(2000, 800, 0.1) == 120
 
 
 def test_calc_annual_revenues():
-    '''Test the function calc_annual_revenues'''
+    """Test the function calc_annual_revenues"""
     assert utils.calc_annual_revenues(800, 0.1) == 80
