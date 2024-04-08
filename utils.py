deb7b059711ee90bb6b1a485a6679805f73c1a74
ReginaTUWien <163769018+ReginaTUWien@users.noreply.github.com>
2024-04-08 20:22:21 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ The parameters are defined in main.py
 
 
 def calc_crf(int_rate, lifetime):
-    """Calculation of capital ercovery factor"""
+    """Calculation of capital recovery factor"""
     crf = ((1 + int_rate) ** lifetime * int_rate) / ((1 + int_rate) ** (lifetime - 1))
     return crf
 
